045661116292d889a583b1be1d81eed413bd28f0
Raúl Fuenzalida <rau.fuenzalida@duocuc.cl>
2025-10-22 11:49:07 -0300

    pruebas unitarias: navbar

--- digipymes360/vite.config.js
+++ digipymes360/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: './'
-})
+  base: "./",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
+});
